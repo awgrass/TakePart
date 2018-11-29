8f8bc9217e99c49d337e469bf16a3717ec3c8f80
KerschbaumerM <martin.kerschi93@gmail.com>
2018-11-29 14:58:19 +0100

    imported firebase configuration and adjusted mvc structure

---
+++ model/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCtulKNvt3Ja3aQOxaDf7UWJ3HtbfIthDQ",
+  authDomain: "wt01-59106.firebaseapp.com",
+  databaseURL: "https://wt01-59106.firebaseio.com",
+  projectId: "wt01-59106",
+  storageBucket: "wt01-59106.appspot.com",
+  messagingSenderId: "600195730115"
+};
+firebase.initializeApp(config);
