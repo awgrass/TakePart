7b0811058594a91cd474ee782d6b645a6225d897
KerschbaumerM <martin.kerschi93@gmail.com>
2019-01-26 15:53:04 +0100

    update statistics

--- model/Statistics.js
+++ model/Statistics.js
@@ -13,7 +13,6 @@ function addStatistic(name, date, registeredAtThisTime){
 
     // Add a new document with a generated id.
     ref.add({
-        name: name,
         date: date,
         participated: 0,
         registeredAtThisTime: registeredAtThisTime
@@ -24,19 +23,33 @@ function addStatistic(name, date, registeredAtThisTime){
         });
 }
 
-function updateStatistic(amount, increase) {
+//Function works but is to be optimized. It decreases or increases amount of participants.
+function updateStatistic(name, date, increase) {
     let ref = firestore
         .collection("courses")
-        .doc(name).collection("statistics");
+        .doc(name).collection("statistics"); //.where("date.seconds", "==", date.getTime() / 1000);
 
-    ref.update({
-        participated: increase ? amount + 1 : amount - 1
-    })
-        .then(function() {
-            console.log("Document successfully updated!");
+        ref.get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                if (date.getTime() / 1000 !== doc.data().date.seconds)return;
+                if (increase && doc.data().participated >= doc.data().registeredAtThisTime)
+                    throw "Cant be more participants than registered people.";
+                else if (!increase && doc.data().participated <= 0)
+                    throw "Cant be less than 0 participants.";
+
+                ref.doc(doc.id).update({
+                    participated: increase ? doc.data().participated + 1 : doc.data().participated - 1
+                })
+                .then(function() {
+                    console.log("Document successfully updated!");
+                })
+                .catch(function(error) {
+                    // The document probably doesn't exist.
+                    console.error("Error updating document: ", error);
+                });
+            });
         })
         .catch(function(error) {
-            // The document probably doesn't exist.
-            console.error("Error updating document: ", error);
+            console.log("Error getting documents: ", error);
         });
 }
