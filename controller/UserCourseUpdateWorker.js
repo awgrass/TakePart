ef13b967c830efe8c718c8cdbfe90b02fb1e9837
Alexander Grass <alexanderwaltergrass@gmail.com>
2019-01-30 16:42:23 +0100

    firebase settings change

--- controller/UserCourseUpdateWorker.js
+++ controller/UserCourseUpdateWorker.js
@@ -1,4 +1,6 @@
-importScripts("https://www.gstatic.com/firebasejs/5.5.9/firebase.js");
+importScripts("https://www.gstatic.com/firebasejs/5.8.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/5.8.1/firebase-firestore.js");
+importScripts("https://www.gstatic.com/firebasejs/5.8.1/firebase-storage.js");
 importScripts('../model/Firebase.js');
 let courseDates = new Map();
 let run = true;
